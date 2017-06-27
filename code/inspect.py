af1424f303b081d2a4f919c51b50d2bca5d14a3d
Steve Harris <doc@steveharris.me>
2017-06-27 23:06:40 +0100

    added basic error checking when creating class

--- code/inspect.py
+++ code/inspect.py
@@ -4,22 +4,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
+# Load data as pandas
 d1d = pd.read_csv(os.path.join('inspectEHR', 'data', 'height.csv'))
 d1d.head()
-print('''Unique episodes ''', len(d1d['id'].unique()), "available")
-len(d1d['id'].unique())
+
 d2d = pd.read_csv(os.path.join('inspectEHR', 'data', 'hrate.csv'))
 d2d.head()
-d1d['val']
-d1d['val'].head()
-d1d.dtypes
 
+# - [ ] @TODO: (2017-06-27) add metadata passing where metadata contains info
+# about how to manage that item
+# - [ ] @TODO: (2017-06-27) add error checking to make sure data contains expected columns
+
+type(d1d)
+r, c = d1d.shape
+r > 0
+s = ' '.join(["Pandas dataframe with", str(r), "rows"])
+print(s)
+
+# Base class
 class DataRaw(object):
     def __init__(self, dt):
+        ''' Initiate and check dataframe with correct rows and dimensions'''
         self.dt = dt
-        self.val = self.dt['val']
-        # print(self.val)
-        print('It worked')
+
+        # Check that this is a pandas data frame
+        assert type(self.dt) == pd.core.frame.DataFrame
+        # Check core column names
+        colnames = ['id', 'site', 'val']
+        assert all([i in self.dt.dtypes for i in colnames])
+        # Check 1 or more row is present
+        self.r, self.c = self.dt.shape
+        assert self.r > 0
+
 
     def inspect(self):
         '''
@@ -35,12 +51,11 @@ class DataRaw(object):
         self.dt['val'].hist(bins=30)
         plt.show()
 
-
-
-
-
-    # def __str__(self):
-    #     return "here's looking at you"
+    def __str__(self):
+        print(self.dt.head())
+        print('\n')
+        s = ' '.join(["Pandas dataframe with", str(self.r), "rows (first 5 shown)"])
+        return s
 
 class Data1D(DataRaw):
     ''' A 1d version of data raw '''
@@ -52,9 +67,12 @@ class Data2D(DataRaw):
     ''' A 2d version of data raw '''
     def __init__(self, dt):
         DataRaw.__init__(self, dt)
-        print('made some 2d')
+        # Check time provided
+        assert 'time' in self.dt.dtypes
+        print(self.dt.head())
 
 heights = DataRaw(d1d)
+print(heights)
 heights.dt.head()
 heights.inspect()
 heights.histogram()
