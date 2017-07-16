a2e70ba8135cbbd8b2788d99a80f8967be902478
Steve Harris <doc@steveharris.me>
2017-07-16 06:59:39 +0100

    working json2feather version

--- code/ccd_json2feather.py
+++ code/ccd_json2feather.py
@@ -22,7 +22,7 @@ from inspectEHR.data_classes import DataRaw, ContMixin, CatMixin
 
 # - [ ] @TODO: (2017-07-15)  make a unique key from ccd (just integer would do)
 #   use this to index the item_tb table
-def extract_all(ccd, d1or2=1, ccd_key = ['site_id', 'episode_id']):
+def extract_all(ccd, d1or2=1, ccd_key = ['site_id', 'episode_id'], progress_marker=False):
     '''Extracts all data in ccd object to single data frame and stores feather object
     Args:
         ccd: ccd object (data frame with data column containing dictionary of dictionaries)
@@ -46,19 +46,24 @@ def extract_all(ccd, d1or2=1, ccd_key = ['site_id', 'episode_id']):
         print('!!! ccd_key should be a list of column names')
         return e
 
-    msg = '*** Extracting all {}d data from {} rows'.format(d1or2, dfin.shape[0])
+    msg = '\n*** Extracting all {}d data from {} rows'.format(d1or2, dfin.shape[0])
     if d1or2 == 1:
         print(msg)
-        return extract_1d(dfin, ccd_key)
+        return extract_1d(dfin, ccd_key, progress_marker)
     else:
         print(msg)
-        return extract_2d(dfin, ccd_key)
+        return extract_2d(dfin, ccd_key, progress_marker)
 
 
-def extract_1d(dfin, ccd_key):
+def extract_1d(dfin, ccd_key, progress_marker):
     df_from_rows = []
+    i = 0
 
     for row in dfin.itertuples():
+        if progress_marker:
+            if i%10 == 0:
+                print(".", end='')
+            i += 1
         df_from_data = []
         row_key = {k:getattr(row, k) for k in ccd_key}
 
@@ -77,43 +82,56 @@ def extract_1d(dfin, ccd_key):
     df = pd.concat(df_from_rows)
     return df
 
-def extract_2d(dfin, ccd_key):
+def extract_2d(dfin, ccd_key, progress_marker):
     df_from_rows = []
+    i = 0
 
     for row in dfin.itertuples():
-        df_from_data = []
+
+        if progress_marker:
+            if i%10 == 0:
+                print(".", end='')
+            i += 1
+
         row_key = {k:getattr(row, k) for k in ccd_key}
 
-        for i, (nhic, d) in enumerate(row.data.items()):
-            if type(d) != dict:  # If 2d then data stored as dict of lists
-                continue
-            else:
-                df = pd.DataFrame.from_dict(d)
-                df['NHICcode'] = nhic
-                df_from_data.append(df)
+        try:
+            df = row.data
+            df = {k:pd.DataFrame.from_dict(v) for k,v in df.items() if type(v) is dict}
+            df = pd.concat(df)
+            df.reset_index(level=0, inplace=True)
+            df.rename(columns={'level_0':'NHICcode'}, inplace=True)
 
-        df = pd.concat(df_from_data)
-        for k,v in row_key.items():
-            df[k] = v
-        df_from_rows.append(df)
+            for k,v in row_key.items():
+                df[k] = v
+            df_from_rows.append(df)
+        except ValueError as e:
+            # unable to concatenate, no data?
+            print('!!! Value error for {}'.format(row_key))
+            print(e)
+            continue
+        except Exception as e:
+            print('!!! Error for {}'.format(row_key))
+            print(e)
+            continue
 
     df = pd.concat(df_from_rows)
     return df
 
 
 if __name__ == "__main__":
-    ccd = CCD(os.path.join('data-raw', 'anon_public_da1000.JSON'), random_sites=True)
-    # ccd = CCD(os.path.join('data-raw', 'anon_internal.JSON'), random_sites=True)
+    # ccd = CCD(os.path.join('data-raw', 'anon_public_da1000.JSON'), random_sites=True)
+    ccd = CCD(os.path.join('data-raw', 'anon_internal.JSON'), random_sites=True)
     ccd_data = ccd.ccd
     refs = load_spec(os.path.join('data-raw', 'N_DataItems.yml'))
 
-    df2d = extract_all(ccd, 2)
+    df2d = extract_all(ccd, 2, progress_marker=True)
     # df2d.shape
     # df2d.head()
     df2d.reset_index(inplace=True)
     df2d.to_feather('data/item_2d.feather')
 
-    df1d = extract_all(ccd, 1)
+    df1d = extract_all(ccd, 1, progress_marker=True)
     # df1d.shape
     # df1d.head()
     df1d.reset_index(inplace=True)
