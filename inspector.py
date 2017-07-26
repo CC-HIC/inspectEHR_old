6c659f426343c31a02ab86079986ddeec4dbc534
Steve Harris <doc@steveharris.me>
2017-07-27 00:22:16 +0100

    added command line parsing

--- inspector.py
+++ inspector.py
@@ -1,5 +1,6 @@
-#! /Users/steve/usr/anaconda3/envs/cchic/bin/python
+import sys
 import os
+import argparse
 import warnings
 import numpy as np
 import seaborn as sns
@@ -26,11 +27,18 @@ def row_generator(NHICcode, ccd, spec, by=False, verbose=False):
         print(NHICcode, cc_item.nrow)
         return cc_item.inspect_row(by=by)
 
-def main():
-    debug_field_limit = None
-    # data_path = 'data/anon_internal.h5'
-    data_path = os.path.join('data', 'anon_public_da1000.h5')
-    spec_path = os.path.join('data-raw', 'N_DataItems.yml')
+def main(args, debug=False):
+
+    if debug:
+        field_limit = 10
+        print('!!! Debugging - will only check first {} fields'.format(field_limit))
+    else:
+        field_limit = None
+
+    data_path         = args.data_path
+    spec_path         = args.spec
+    results_path      = args.to
+    bysite            = args.bysite
 
     spec = load_spec(spec_path)
     spec_df = pd.DataFrame(spec).T
@@ -39,10 +47,10 @@ def main():
 
     non_text_fields = ['numeric', 'list', 'list / logical', 'Logical']
     fields2check = {k:v for k,v in spec.items() if v['Datatype'] in non_text_fields}
-    fields = [k for k in fields2check.keys()][:debug_field_limit]
+    fields = [k for k in fields2check.keys()][:field_limit]
 
     # parentheses turn the following into a generator expression
-    rows = list((row_generator(f, ccd=ccd, spec=spec, by=False, verbose=True) for f in fields))
+    rows = list((row_generator(f, ccd=ccd, spec=spec, by=bysite, verbose=True) for f in fields))
 
     # Convert list of dataframes to single data frame
     results = pd.concat(rows)
@@ -55,7 +63,41 @@ def main():
 
     col_order = "NHICcode site_id dataItem level count nunique n pct min 25% 50% 75% max mean std coerced_values miss_by_episode gap_period gap_start gap_stop".split()
     # results[col_order].to_clipboard()
-    results[col_order].to_csv('data/results.csv')
+    results[col_order].to_csv(results_path)
+
+def cli():
+    ''' Command line interface for running script '''
+
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description='Report data quality for CCD object'
+    )
+
+    parser.add_argument('data_path',
+                        help='JSON or hd5 file to be parsed')
+
+    parser.add_argument('-s', '--spec',
+                        default = 'N_DataItems.yml',
+                        help='Data specification')
+
+    parser.add_argument('-t', '--to',
+                        default = 'inspector.csv',
+                        help='CSV file with results')
+
+    parser.add_argument('--bysite',
+                        action='store_true',
+                        help='Inspection stratified by site')
+
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
-    main()
+    args = cli()
+    
+    # interactive = True
+    # if interactive:
+    #     args.data_path         = 'data/anon_public_da1000.h5'
+    # print(args)
+    # main(args, debug=True)
+
+    main(args)
